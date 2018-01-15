cdeda0af991bd12ff7ffa8c5c14adaa64cb925f6
Chris Chasm <chris@chasm.solutions>
2018-01-15 13:35:19 -0700

    Corrected file extension and add initial plugin root info

--- zume-android-app.php
+++ zume-android-app.php
@@ -9,3 +9,17 @@ Version: 0.1
 Text Domain: zume
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
+
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+} // Exit if accessed directly
+
+// Test if zume_multilingual site exists
+if( ! function_exists( 'zume_multilingual_exists') ) {
+    exit;
+}
+
+/**
+ * Add extension code below.
+ */
